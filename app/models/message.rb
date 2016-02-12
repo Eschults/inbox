b6596295a27afdbdec6c6cf2aafda8449b01573c
Edward Schults <edward.schults@gmail.com>
2016-02-12 13:51:30 +0100

    ready for React

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,10 @@ class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :conversation
 
+  validates :content, presence: true
+  validates :conversation, presence: true
+  validates :user, inclusion: { in: :users }
+
   def mark_as_read
     self.read_at = DateTime.now
     self.save
@@ -10,5 +14,9 @@ class Message < ActiveRecord::Base
   def mark_as_unread
     self.read_at = nil
     self.save
+  end
+
+  def users
+    conversation.users
   end
 end
