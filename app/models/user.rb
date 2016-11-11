18c1f114723df46e9754e58f2f8b7c34f2f6e54a
Edward Schults <edward.schults@gmail.com>
2016-11-11 16:22:27 +0100

    added dependent nullify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :messages
+  has_many :messages, dependent: :nullify
   devise :omniauthable, omniauth_providers: [:facebook]
 
   def conversations
